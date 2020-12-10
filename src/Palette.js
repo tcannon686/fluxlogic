eeac39626e2109e7cfabd3984c22f04960c1f0b8
Thomas Cannon <tom-cannon@playcannon.com>
2020-12-10 12:28:08 -0800

    Added inverted gates to palette

--- src/Palette.js
+++ src/Palette.js
@@ -11,6 +11,10 @@ import ZeroGateSvg from './assets/zero-gate.svg'
 import LedSvg from './assets/led.svg'
 import BufferGateSvg from './assets/buffer-gate.svg'
 
+import PinInvertedSvg from './assets/pin-inverted.svg'
+
+import Wire from './Wire'
+
 import logic from './logic'
 
 function PaletteItem (props) {
@@ -18,7 +22,49 @@ function PaletteItem (props) {
     <Grid item xs={3}>
       <Tooltip title={`Add ${props.text}`}>
         <Button onClick={props.onClick}>
-          <img className='paletteItemImage' src={props.svg} alt='' />
+          <div
+            style={{
+              position: 'relative',
+              width: '0.5in',
+              height: '0.5in',
+              overflow: 'hidden'
+            }}
+          >
+            {
+              props.inverted && (
+                <Wire
+                  x0={0.25}
+                  y0={0.25}
+                  x1={0.5 - 0.0625}
+                  y1={0.25}
+                />
+              )
+            }
+
+            {
+              props.inverted && (
+                <img
+                  src={PinInvertedSvg}
+                  alt=''
+                  style={{
+                    position: 'absolute',
+                    left: `${0.5 - 0.125}in`,
+                    top: `${0.25 - 0.0625}in`
+                  }}
+                />
+              )
+            }
+
+            <img
+              src={props.svg}
+              alt=''
+              style={{
+                position: 'absolute',
+                left: props.inverted ? '-0.0625in' : 0,
+                top: 0
+              }}
+            />
+          </div>
         </Button>
       </Tooltip>
     </Grid>
@@ -33,17 +79,33 @@ function PaletteItem (props) {
  *           parameter is a function that constructs a new logic gate.
  */
 function Palette (props) {
+  const invert = (gate) => {
+    gate.outputs[0].isInverted = true
+    return gate
+  }
   return (
     <Grid container className={props.className}>
       <PaletteItem
         svg={AndGateSvg}
-        text='and gate'
+        text='AND gate'
         onClick={() => props.onSelect(logic.andGate)}
       />
       <PaletteItem
+        svg={AndGateSvg}
+        text='NAND gate'
+        onClick={() => props.onSelect(() => invert(logic.andGate()))}
+        inverted
+      />
+      <PaletteItem
         svg={OrGateSvg}
-        text='or gate'
+        text='OR gate'
         onClick={() => props.onSelect(logic.orGate)}
+      />
+      <PaletteItem
+        svg={OrGateSvg}
+        text='NOR gate'
+        onClick={() => props.onSelect(() => invert(logic.orGate()))}
+        inverted
       />
       <PaletteItem
         svg={ZeroGateSvg}
@@ -59,6 +121,12 @@ function Palette (props) {
         svg={BufferGateSvg}
         text='buffer'
         onClick={() => props.onSelect(() => logic.buffer())}
+      />
+      <PaletteItem
+        svg={BufferGateSvg}
+        text='inverter'
+        onClick={() => props.onSelect(() => invert(logic.buffer()))}
+        inverted
       />
       <PaletteItem
         svg={LedSvg}
