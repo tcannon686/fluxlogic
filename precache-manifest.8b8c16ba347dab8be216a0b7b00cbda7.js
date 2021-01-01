e6093b27211de5493ab62250caa4a7fb97e01069
Thomas Cannon <tom-cannon@playcannon.com>
2021-01-01 11:59:25 -0800

    Updates

---
+++ precache-manifest.8b8c16ba347dab8be216a0b7b00cbda7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072c33747edb3b8831a791b9e720d5ea",
+    "url": "/fluxlogic/377e48f1ef76fbe460e4.worker.js"
+  },
+  {
+    "revision": "7243713e72d1fd9605a10e02eb5f679f",
+    "url": "/fluxlogic/index.html"
+  },
+  {
+    "revision": "742eb9fe852b1fba115b",
+    "url": "/fluxlogic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "26c7a5b236181a0ab2fe",
+    "url": "/fluxlogic/static/js/2.eea89885.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/fluxlogic/static/js/2.eea89885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742eb9fe852b1fba115b",
+    "url": "/fluxlogic/static/js/main.ad6701b7.chunk.js"
+  },
+  {
+    "revision": "a854bc0ac6c336b98e85",
+    "url": "/fluxlogic/static/js/runtime-main.e83e6801.js"
+  },
+  {
+    "revision": "b39df76c0bb23c0a218fcd51d8fe4888",
+    "url": "/fluxlogic/static/media/and-gate.b39df76c.svg"
+  },
+  {
+    "revision": "96d07f6ac3aa0ee95e20379020e7b4b7",
+    "url": "/fluxlogic/static/media/buffer-gate.96d07f6a.svg"
+  },
+  {
+    "revision": "ff1875d8d109fd8c98ed3f30fa1b1e15",
+    "url": "/fluxlogic/static/media/led-glow.ff1875d8.svg"
+  },
+  {
+    "revision": "788936c79bbc8fb81188d09e924e2dec",
+    "url": "/fluxlogic/static/media/led.788936c7.svg"
+  },
+  {
+    "revision": "ac34b99c2af72bff1fb7bfaf3dc83182",
+    "url": "/fluxlogic/static/media/one-gate.ac34b99c.svg"
+  },
+  {
+    "revision": "fb8a5ebb65ba06c7734f0656611abb8d",
+    "url": "/fluxlogic/static/media/open-pin-inverted.fb8a5ebb.svg"
+  },
+  {
+    "revision": "402b8aa68beec0f89853d9f5ca68ede0",
+    "url": "/fluxlogic/static/media/open-pin.402b8aa6.svg"
+  },
+  {
+    "revision": "ce29e39543d87fbb493aecc3bc3ec935",
+    "url": "/fluxlogic/static/media/or-gate.ce29e395.svg"
+  },
+  {
+    "revision": "25b874496f8a8f7deb184e1da98fd8bc",
+    "url": "/fluxlogic/static/media/pin-inverted.25b87449.svg"
+  },
+  {
+    "revision": "4c8eaca63818c882bf2231def965953b",
+    "url": "/fluxlogic/static/media/pin.4c8eaca6.svg"
+  },
+  {
+    "revision": "34ee3b2efdb27ace3ae9f064d5bfafe6",
+    "url": "/fluxlogic/static/media/receiver.34ee3b2e.svg"
+  },
+  {
+    "revision": "737fb0fa2cec2c86278830fbad50c020",
+    "url": "/fluxlogic/static/media/sender.737fb0fa.svg"
+  },
+  {
+    "revision": "b1ab986cf7586b3d7067132445770c95",
+    "url": "/fluxlogic/static/media/switch-off.b1ab986c.svg"
+  },
+  {
+    "revision": "37d11db0cbabb318d4a94882c1b88d0e",
+    "url": "/fluxlogic/static/media/switch-on.37d11db0.svg"
+  },
+  {
+    "revision": "12bb51fa3c6ca72c84013bbe8f89a571",
+    "url": "/fluxlogic/static/media/xor-gate.12bb51fa.svg"
+  },
+  {
+    "revision": "d126f49d62f6b216fb9262c312155988",
+    "url": "/fluxlogic/static/media/zero-gate.d126f49d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mml2/precache-manifest.29abbb524e2eeb9812c7f4bd819739ec.js"
+  "/fluxlogic/precache-manifest.8b8c16ba347dab8be216a0b7b00cbda7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mml2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fluxlogic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
