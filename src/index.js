9a7feb0b745e3902cdccfeee60eafeb85ac91bc4
Thomas Cannon <tom-cannon@playcannon.com>
2021-01-01 17:57:20 -0800

    Updated Material UI theme.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#5f5fc4',
+      main: '#283593',
+      dark: '#001064',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ffbd45',
+      main: '#fb8c00',
+      dark: '#c25e00',
+      contrastText: '#fff'
+    }
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
